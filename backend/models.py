cd96eb1d4ecb4d3dc4fd8f831efaaf79452e8207
frokofaron <mineev96@gmail.com>
2020-12-01 20:05:24 +0300

    добавлены модели

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+from django.db.models import Model, CharField, TextField, IntegerField, ForeignKey, ManyToManyField, CASCADE
 
-# Create your models here.
+
+class User(Model):
+    username = CharField(max_length=50)
+    nickname = CharField(max_length=50)
+    password = CharField(max_length=50)
+
+
+class Question(Model):
+    order = IntegerField()
+    text = TextField()
+    answer = TextField()
+
+
+class Theme(Model):
+    questions = ManyToManyField(Question)
+
+
+class Round(Model):
+    themes = ManyToManyField(Theme)
+
+
+class Game(Model):
+    author = ForeignKey(User,
+                        related_name='%(class)s_nickname',
+                        on_delete=CASCADE)
+    rounds = ManyToManyField(Round)
+    final_round = ForeignKey(Question, on_delete=CASCADE)
+
+
+class GameSession(Model):
+    creator = ForeignKey(User,
+                         related_name='%(class)s_nickname',
+                         on_delete=CASCADE)
+    game = ForeignKey(Game, on_delete=CASCADE)
+    max_players = IntegerField()
+    players = ManyToManyField(User)
