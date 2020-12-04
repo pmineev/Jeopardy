b92b98384cdc8281c17a216d8e94e5c54ce439d3
frokofaron <mineev96@gmail.com>
2020-12-04 17:05:00 +0300

    добавлены регистрация пользователей

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from backend.models import UserProfile
 
-# Create your views here.
+
+def register(request):
+    print(request.body)
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        print(body)
+
+        username = body['username']
+        nickname = body['nickname'] if 'nickname' in body else username
+        password = body['password']
+
+        if username and password:
+            if User.objects.filter(username=username).exists():
+                return HttpResponse(status=409)
+            else:
+                user = UserProfile(user=User.objects.create_user(username=username, password=password),
+                                   nickname=nickname)
+                user.save()
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=401)
--- jeopardy/urls.py
+++ jeopardy/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from backend.views import register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', register),
 ]
