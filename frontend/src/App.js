fe2eb7e67fc3142971e742f64d76d1da56af3832
frokofaron <mineev96@gmail.com>
2021-01-22 02:47:09 +0300

    добавлен роутинг

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import './App.css'
 import SignupForm from "./registration";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 function App() {
-    return <SignupForm/>;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/register">
+                    <SignupForm/>
+                </Route>
+                <Route path="/login">
+                    {/*<LoginForm/>*/}
+                </Route>
+                <Route path="/games">
+                    {/*<GamesList/>*/}
+                </Route>
+                <Route path="/gameSessions">
+                    {/*<GameSessionsList/>*/}
+                </Route>
+                <Route path="/game">
+                    {/*<Game/>*/}
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
