83f1801bd7ae75653a1a448d9c16a154069eea2c
frokofaron <mineev96@gmail.com>
2021-01-24 18:42:13 +0300

    добавлено обновление токена доступа

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,11 +1,49 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
+axios.interceptors.request.use(
+    config => {
+        if (!(config.url === '/sessions/')) {
+            const access_token = localStorage.getItem('access_token');
+            config.headers['Authorization'] = `Bearer ${access_token}`;
+        }
+
+        return config;
+    }
+);
+
+axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response.status !== 401) {
+            return Promise.reject(error);
+        }
+
+        const refresh_token = localStorage.getItem('refresh_token');
+        if (refresh_token) {
+            axios.post('/sessions/new_token/', {
+                refresh: refresh_token
+            })
+                .then(response => {
+                    localStorage.setItem('access_token', response.data.access)
+                })
+        } else {
+
+        }
+
+        console.log(error.response);
+
+    }
+);
+
 
 class AuthService {
 
     register(credentials) {
-        const url = `${API_URL}/users/`;
+        const url = '/users/';
         return axios.post(url, {
             username: credentials.username,
             nickname: credentials.nickname,
@@ -22,7 +60,7 @@ class AuthService {
     }
 
     login(credentials) {
-        const url = `${API_URL}/sessions/`;
+        const url = '/sessions/';
         return axios
             .post(url, {
                 username: credentials.username,
