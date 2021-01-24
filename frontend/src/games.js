988f82e7266c75fae6e4b22794c924620ba7ddcb
frokofaron <mineev96@gmail.com>
2021-01-24 05:11:14 +0300

    добавлен компонент списка игр

--- frontend/src/games.js
+++ frontend/src/games.js
@@ -1,8 +1,59 @@
-const GamesList = () => {
-    return (
-        <header>Игры</header>
+//import {useState} from 'react'
+//import {GameListService} from "./services";
 
+//const gameListService = new GameListService();
+
+const GameDescription = (props) => {
+    const descr = props.descr;
+    console.log(descr.name);
+
+    return (
+        <tr>
+            <td>{descr.author}</td>
+            <td>{descr.name}</td>
+            <td>{descr.rounds_count}</td>
+            <td>
+                <button>Играть</button>
+            </td>
+        </tr>
+    );
+};
+const GameList = () => {
+    // const [gameDescriptions, setGameDescriptions] = useState(0);
+    // gameListService.getDescriptions()
+    //     .then((result) => setGameDescriptions(result))
+    const gameDescriptions = [
+        {
+            "name": "Тестовая",
+            "author": "frokouyguyguygfaron",
+            "rounds_count": 3
+        },
+        {
+            "name": "Тестовейшая",
+            "author": "faron",
+            "rounds_count": 2
+        }
+    ]
+    return (
+        <>
+            <header>Игры</header>
+
+            <table className="games-table">
+                <thead key="games-table-head">
+                <tr>
+                    <th>Автор</th>
+                    <th>Название</th>
+                    <th>Раунды</th>
+                </tr>
+                </thead>
+                <tbody>
+                {gameDescriptions.map(descr =>
+                    <GameDescription key={descr.name} descr={descr}/>
+                )}
+                </tbody>
+            </table>
+        </>
     );
 };
 
-export default GamesList;
+export default GameList;
