b19246cf6a84859812638511f7b80758ea8578c5
frokofaron <mineev96@gmail.com>
2021-01-26 04:59:26 +0300

    добавлен компонент модального окна создания новой игры

--- frontend/src/games.js
+++ frontend/src/games.js
@@ -1,7 +1,65 @@
 import {useEffect, useState} from 'react'
 import {GameListService} from "./services";
+import Modal from 'react-modal';
+import {Form, Formik} from "formik";
+import * as Yup from "yup";
+import {TextInput} from "./inputs";
 
 const gameListService = new GameListService();
+
+const AddGameForm = () => {
+    return (
+        <>
+            <Formik
+                initialValues={{
+                    name: '',
+                    rounds_count: '3',
+                    questions_count: '5',
+                }}
+                validationSchema={Yup.object({
+                    name: Yup.string()
+                        .required('Обязательное поле'),
+                    rounds_count: Yup.number()
+                        .required('Обязательное поле')
+                        .min(2, 'Не менее 2 раундов')
+                        .max(10, 'Не более 10 раундов')
+                        .typeError('Введите число')
+                        .integer('Так тоже не прокатит'),
+                    questions_count: Yup.number()
+                        .required('Обязательное поле')
+                        .min(1, 'Не менее 1 вопроса')
+                        .max(10, 'Не более 10 вепросов')
+                        .typeError('Введите число')
+                        .integer('Так тоже не прокатит')
+                })}
+                onSubmit={(values, {setSubmitting, setErrors}) => {
+
+                }}
+            >
+                <Form>
+                    <header>Новая игра</header>
+                    <TextInput
+                        label="Название"
+                        name="name"
+                        type="text"
+                    />
+                    <TextInput
+                        label="Количество раундов"
+                        name="rounds_count"
+                        type="text"
+                    />
+                    <TextInput
+                        label="Количество вопросов в теме"
+                        name="questions_count"
+                        type="text"
+                    />
+
+                    <button type="submit">Создать игру</button>
+                </Form>
+            </Formik>
+        </>
+    );
+};
 
 const GameDescription = (props) => {
     const descr = props.descr;
@@ -19,6 +77,7 @@ const GameDescription = (props) => {
 };
 const GameList = () => {
     const [gameDescriptions, setGameDescriptions] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
         gameListService.getDescriptions()
@@ -43,8 +102,21 @@ const GameList = () => {
                 {gameDescriptions.map(descr =>
                     <GameDescription key={descr.name} descr={descr}/>
                 )}
+                <button onClick={() => setIsModalOpen(true)}>Создать новую игру</button>
                 </tbody>
             </table>
+
+            <Modal
+                className='modal add-game'
+                ovrlayClassName='overlay modal add-game'
+                isOpen={isModalOpen}
+                onRequestClose={() => setIsModalOpen(false)}
+                ariaHideApp={false}
+
+            >
+                <AddGameForm/>
+
+            </Modal>
         </>
     );
 };
