b40bab85215f30ba431e4e3a6b52dadcc21a432d
frokofaron <mineev96@gmail.com>
2021-02-10 01:44:53 +0300

    изменена структура уведомления об ответе игрока

--- backend/notifiers.py
+++ backend/notifiers.py
@@ -81,14 +81,13 @@ class GameSessionNotifier:
     def player_answered(self, game_session_id, username, answer, is_correct=True):
         player = self.repo.get_player(game_session_id, username)
         player_dict = PlayerSerializer(player).data
-        player_dict.update(
-            {
-                'answer': answer,
-                'is_correct': is_correct
-            }
-        )
+        answer_dict = {
+            'player': player_dict,
+            'text': answer,
+            'is_correct': is_correct
+        }
 
-        self._notify(str(game_session_id), player_dict, 'game_session_event', 'player_answered')
+        self._notify(str(game_session_id), answer_dict, 'game_session_event', 'player_answered')
 
     def question_timeout(self, game_session_id):
         answer = self.repo.get_current_question_answer(game_session_id)
