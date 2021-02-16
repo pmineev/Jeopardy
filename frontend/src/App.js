a7153c74f310d972e475961ca73d029a1920c6c7
frokofaron <mineev96@gmail.com>
2021-02-16 16:25:29 +0300

    добавлено перенаправление пользователей со стартовой страницы

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, {Fragment} from "react";
 import './App.css'
 import {RegisterForm} from "./registration";
-import {PrivateRoute, ProvideAuth} from "./auth";
+import {PrivateRoute, useAuth} from "./auth";
 import LoginForm from "./login";
 import GameList from "./games";
 import Header from "./header";
@@ -10,41 +10,42 @@ import AddGame from "./addGame";
 import UserProfileForm from "./userProfile";
 import Game from "./game";
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 function App() {
+    const auth = useAuth();
     return (
-        <ProvideAuth>
-            <Router>
-                <Switch>
-                    <Route path="/register">
-                        <RegisterForm/>
-                    </Route>
-                    <Route path="/login">
-                        <LoginForm/>
-                    </Route>
-                    <Fragment>
-                        <Header/>
-                        <PrivateRoute exact path="/user">
-                            <UserProfileForm/>
-                        </PrivateRoute>
-                        <PrivateRoute exact path="/games">
-                            <GameList/>
-                        </PrivateRoute>
-                        <PrivateRoute exact path="/games/new">
-                            <AddGame/>
-                        </PrivateRoute>
-                        <PrivateRoute path="/lobby">
-                            <Lobby/>
-                        </PrivateRoute>
-                        <PrivateRoute path="/game">
-                            <Game/>
-                        </PrivateRoute>
-                    </Fragment>
-                </Switch>
-            </Router>
-        </ProvideAuth>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    {auth.isAuthenticated ? <Redirect to="/games"/> : <Redirect to="/login"/>}
+                </Route>
+                <Route path="/register">
+                    <RegisterForm/>
+                </Route>
+                <Route path="/login">
+                    <LoginForm/>
+                </Route>
+                <Fragment>
+                    <Header/>
+                    <PrivateRoute exact path="/user">
+                        <UserProfileForm/>
+                    </PrivateRoute>
+                    <PrivateRoute exact path="/games">
+                        <GameList/>
+                    </PrivateRoute>
+                    <PrivateRoute exact path="/games/new">
+                        <AddGame/>
+                    </PrivateRoute>
+                    <PrivateRoute path="/lobby">
+                        <Lobby/>
+                    </PrivateRoute>
+                    <PrivateRoute path="/game">
+                        <Game/>
+                    </PrivateRoute>
+                </Fragment>
+            </Switch>
+        </Router>
     );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ProvideAuth} from "./auth";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ProvideAuth>
+            <App/>
+        </ProvideAuth>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
