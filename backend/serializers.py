eadee42f3213c9b4e462b4c554f56576182974eb
frokofaron <mineev96@gmail.com>
2021-10-16 08:19:55 +0300

    добавлены сериализаторы запросов

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,5 @@
 from django_enum_choices.serializers import EnumChoiceField
-from rest_framework.serializers import Serializer, CharField, IntegerField, BooleanField
+from rest_framework.serializers import Serializer, CharField, IntegerField, BooleanField, ListField
 
 from backend.enums import State
 
@@ -58,3 +58,54 @@ class GameStateSerializer(Serializer):
     current_round = RoundDescriptionSerializer(required=False)
     current_player = PlayerSerializer(required=False)
     current_question = QuestionDescriptionSerializer(required=False)
+
+
+class RegisterUserCredentialsSerializer(Serializer):
+    username = CharField()
+    nickname = CharField(required=False)
+    password = CharField()
+
+
+class LoginUserCredentialsSerializer(Serializer):
+    username = CharField()
+    password = CharField()
+
+
+class ChangeUserCredentialsSerializer(Serializer):
+    nickname = CharField(required=False)
+    password = CharField(required=False)
+
+
+class QuestionSerializer(Serializer):
+    text = CharField()
+    answer = CharField()
+    value = IntegerField(min_value=0)
+
+
+class ThemeSerializer(Serializer):
+    name = CharField()
+    questions = ListField(child=QuestionSerializer())
+
+
+class RoundSerializer(Serializer):
+    themes = ListField(child=ThemeSerializer())
+
+
+class GameSerializer(Serializer):
+    name = CharField()
+    rounds = ListField(child=RoundSerializer())
+    finalRound = QuestionSerializer(source='final_round')
+
+
+class CreateGameSessionSerializer(Serializer):
+    gameName = CharField(source='game_name')
+    maxPlayers = IntegerField(source='max_players')
+
+
+class QuestionChoiceSerializer(Serializer):
+    themeOrder = IntegerField(min_value=0, source='theme_order')
+    questionOrder = IntegerField(min_value=0, source='question_order')
+
+
+class PlayerAnswerSerializer(Serializer):
+    answer = CharField()
