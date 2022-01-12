546a4f5a7e5fde8fd65aab4a8a89a16bab6c0a47
frokofaron <mineev96@gmail.com>
2022-01-13 01:17:28 +0300

    удалены лишние методы

--- backend/consumers.py
+++ backend/consumers.py
@@ -5,24 +5,7 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class LobbyConsumer(WebsocketConsumer):
-    group_name = 'lobby'
-
-    def connect(self):
-        async_to_sync(self.channel_layer.group_add)(LobbyConsumer.group_name, self.channel_name)
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(LobbyConsumer.group_name, self.channel_name)
-
-        self.close()
-
-    def receive_json(self, content, **kvargs):
-        async_to_sync(self.channel_layer.group_send)(
-            LobbyConsumer.group_name,
-            {
-                'type': 'lobby_event'
-            })
+    groups = ['lobby']
 
     def lobby_event(self, event):
         event.pop('type')
@@ -30,9 +13,6 @@ class LobbyConsumer(WebsocketConsumer):
 
 
 class GameSessionConsumer(WebsocketConsumer):
-    def __init__(self):
-        super().__init__()
-
     def connect(self):
         self.group_name = self.scope['url_route']['kwargs']['game_session_id']
 
