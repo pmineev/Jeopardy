6fcb5dc41e5aea07c158868dbc7a69d8530ef4b5
frokofaron <mineev96@gmail.com>
2022-02-10 10:03:33 +0300

    добавлено использование тостов

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -12,41 +12,47 @@ import GameList from "../GameList/GameList";
 import AddGame from "../AddGame/AddGame";
 import Lobby from "../Lobby/Lobby";
 import Game from "../Game/Game";
+import Toast from "../Toast/Toast";
 
 function App() {
     const auth = useAuth();
+
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    {auth.isAuthenticated ? <Redirect to="/games"/> : <Redirect to="/login"/>}
-                </Route>
-                <Route path="/register">
-                    <RegisterForm/>
-                </Route>
-                <Route path="/login">
-                    <LoginForm/>
-                </Route>
-                <Fragment>
-                    <Header/>
-                    <PrivateRoute exact path="/user">
-                        <UserProfileForm/>
-                    </PrivateRoute>
-                    <PrivateRoute exact path="/games">
-                        <GameList/>
-                    </PrivateRoute>
-                    <PrivateRoute exact path="/games/new">
-                        <AddGame/>
-                    </PrivateRoute>
-                    <PrivateRoute path="/lobby">
-                        <Lobby/>
-                    </PrivateRoute>
-                    <PrivateRoute path="/game">
-                        <Game/>
-                    </PrivateRoute>
-                </Fragment>
-            </Switch>
-        </Router>
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        {auth.isAuthenticated ? <Redirect to="/games"/> : <Redirect to="/login"/>}
+                    </Route>
+                    <Route path="/register">
+                        <RegisterForm/>
+                    </Route>
+                    <Route path="/login">
+                        <LoginForm/>
+                    </Route>
+                    <Fragment>
+                        <Header/>
+                        <PrivateRoute exact path="/user">
+                            <UserProfileForm/>
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/games">
+                            <GameList/>
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/games/new">
+                            <AddGame/>
+                        </PrivateRoute>
+                        <PrivateRoute path="/lobby">
+                            <Lobby/>
+                        </PrivateRoute>
+                        <PrivateRoute path="/game">
+                            <Game/>
+                        </PrivateRoute>
+                    </Fragment>
+                </Switch>
+            </Router>
+            <Toast/>
+        </>
+
     );
 }
 
