fe7cfb4627725abf7213b917fb754db8f694ab33
frokofaron <mineev96@gmail.com>
2022-02-15 23:51:45 +0300

    добавлено заполнение консьюмера игры при запуске сервера

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -6,4 +6,12 @@ class BackendConfig(AppConfig):
 
     def ready(self):
         from backend.modules.game_session.event_handlers import register_handlers
+
+        from backend.infra.models import ORMPlayer
+        from backend.infra.consumers import GameSessionConsumer
+
         register_handlers()
+
+        for orm_player in ORMPlayer.objects.all():
+            GameSessionConsumer.add_user(username=orm_player.user.user.username,
+                                         game_session_id=orm_player.game_session.pk)
