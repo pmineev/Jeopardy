9fe8895cfde7c92145bb555cd1ca4e550011fcc9
frokofaron <mineev96@gmail.com>
2022-02-21 21:54:27 +0300

    если пользователь не залогинен, то перенаправление на страницу логина будет без сохранения в истории

--- frontend/src/common/auth/PrivateRoute.jsx
+++ frontend/src/common/auth/PrivateRoute.jsx
@@ -6,7 +6,7 @@ const PrivateRoute = ({children}) => {
     return (
         isAuthenticated()
             ? children
-            : <Navigate to="/login"/>
+            : <Navigate to="/login" replace/>
     );
 }
 
