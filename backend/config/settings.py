fc583381da50877b08fa75ae4c3f0b1cb8d22afb
frokofaron <mineev96@gmail.com>
2022-02-28 21:01:52 +0300

    переход на postgres

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,8 +62,12 @@ ASGI_APPLICATION = 'backend.config.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DBNAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
